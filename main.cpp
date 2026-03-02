226ed3881793cc0706fb96d866fd896e2f698f52
mikii521 <miklos.gabriel2@gmail.com>
2026-03-02 17:09:24 +0100

    kijavított kód

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // hibás konstansnév (NELEMENTS nincs definiálva)
-    std::cout << '1-100 ertekek duplazasa' // aposztróf helyett dupla idézőjel, hiányzik pontosvessző
-    for (int i = 0;) // hiányzik a feltétel és az inkrement
+    int *b = new int[N_ELEMENTS]; // helyes konstansnév
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // helyes idézőjel és pontosvessző
+    for (int i = 0; i < N_ELEMENTS; i++) // feltétel és inkrement hozzáadva
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // a feltétel hibás (mindig false lesz)
+    for (int i = 0; i < N_ELEMENTS; i++) // helyes feltétel
     {
-        std::cout << "Ertek:" // hiányzik pontosvessző és kiírandó érték
+        std::cout << "Ertek: " << b[i] << std::endl; // kiíratjuk a tömb elemeit
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // nincs inicializálva használat előtt
-    for (int i = 0; i < N_ELEMENTS, i++) // vessző helyett pontosvessző kell
+    int atlag = 0; // inicializálva
+    for (int i = 0; i < N_ELEMENTS; i++) // javított elválasztó
     {
-        atlag += b[i] // hiányzik pontosvessző
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
