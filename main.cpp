a7fb4c4eef823cb3343911c58144e33f5d6df8cf
mikii521 <miklos.gabriel2@gmail.com>
2026-03-02 17:08:27 +0100

    hibák ki kommentelve

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // hibás konstansnév (NELEMENTS nincs definiálva)
+    std::cout << '1-100 ertekek duplazasa' // aposztróf helyett dupla idézőjel, hiányzik pontosvessző
+    for (int i = 0;) // hiányzik a feltétel és az inkrement
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // a feltétel hibás (mindig false lesz)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // hiányzik pontosvessző és kiírandó érték
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // nincs inicializálva használat előtt
+    for (int i = 0; i < N_ELEMENTS, i++) // vessző helyett pontosvessző kell
     {
-        atlag += b[i]
+        atlag += b[i] // hiányzik pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
